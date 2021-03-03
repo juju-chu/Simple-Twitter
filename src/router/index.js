2db2bf2d15764e339e6215b24a3d4cc708acfd80
Mike <win0362002@hotmail.com>
2021-03-03 21:48:31 +0800

    Update router, add root and signin path

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
 import Tweets from '../views/Tweets.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/signin'
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
   {
     path: '/tweets',
     name: 'tweets',
