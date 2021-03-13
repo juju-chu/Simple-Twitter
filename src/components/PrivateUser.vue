84528ff162f990fcdb223cb46f618597105cf102
Juju <jujupp1116@gmail.com>
2021-03-14 00:05:43 +0800

    create PrivateUser.vue base on  OnlineUser.vue

---
+++ src/components/PrivateUser.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <router-link :to="{ name: 'user', params: { id: user.id } }">
+        <img class="avatar" :src="user.avatar" />
+      </router-link>
+      <router-link :to="{ name: 'user', params: { id: user.id } }">
+        <span class="name">{{ user.name }}</span>
+      </router-link>
+      <router-link :to="{ name: 'user', params: { id: user.id } }">
+        <span class="account">@{{ user.account }}</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OnlineUser',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+  width: 378px;
+  height: 80px;
+}
+
+.avatar {
+  /* margin: 15px 0 15px 15px; */
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+.name {
+  margin-left: 10px;
+  font-weight: bold;
+  font-size: 15px;
+}
+
+.account {
+  margin-left: 5px;
+  font-weight: 500;
+  font-size: 15px;
+  color: #657786;
+}
+</style>
