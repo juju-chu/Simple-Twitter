459449c8dece4031dd481c86c8f862acbb30e566
Mike <win0362002@hotmail.com>
2021-03-13 14:48:13 +0800

    Add socket.io initial flow to setup websocket connection

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from './App.vue'
 import router from './router'
 import './assets/application.css'
 import store from './store'
+// For vue - socket.io 
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
+
+const token = localStorage.getItem('token')
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('https://mighty-spire-48914.herokuapp.com', {
+    reconnectionDelayMax: 10000,
+    auth: {
+      token: token
+    },
+    query: {
+      "my-key": "my-value"
+    }
+  }),
+  vuex: {
+    store,
+    actionPrefix: "SOCKET_",
+    mutationPrefix: "SOCKET_"
+  }
+})
+)
 
 Vue.config.productionTip = false
 
