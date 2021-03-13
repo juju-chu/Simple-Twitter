7c960503a33a9dd77eb187a1670285f81ed850ae
Juju <jujupp1116@gmail.com>
2021-03-13 11:50:37 +0800

    update grid, and add public room

--- src/views/PublicMessage.vue
+++ src/views/PublicMessage.vue
@@ -2,27 +2,42 @@
   <div class="wrapper">
     <SideBar class="side-bar" @after-post-tweet="afterPostTweet" />
 
-    <div class="center-column">
-      <div class="header">
-        <div class="header-title">上線使用者 ({{ onlineCount }})</div>
-      </div>
-
-      <div class="onlineUser-wrapper">
-        <OnlineUser
-          class="onlineUser"
-          v-for="user in users"
-          :key="user.id"
-          :user="user"
-        />
-      </div>
+    <div class="header-users">上線使用者 ({{ onlineCount }})</div>
+    <div class="onlineUser-wrapper">
+      <OnlineUser
+        class="onlineUser"
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+      />
     </div>
 
-    <div class="right-column">
-      <div class="header">
-        <div class="header-title">公開聊天室</div>
-      </div>
-      <!-- <PublicRoom class="publicRoom" /> -->
-    </div>
+    <div class="header-room">公開聊天室</div>
+    <div class="messages-wrapper">messages</div>
+    <input
+      class="input-text"
+      type="text"
+      placeholder="輸入訊息..."
+      v-model="message"
+    />
+    <button class="input-icon">
+      <svg
+        class="svg-inline--fa fa-paper-plane fa-w-16"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="far"
+        data-icon="paper-plane"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        data-fa-i2svg=""
+      >
+        <path
+          fill="currentColor"
+          d="M440 6.5L24 246.4c-34.4 19.9-31.1 70.8 5.7 85.9L144 379.6V464c0 46.4 59.2 65.5 86.6 28.6l43.8-59.1 111.9 46.2c5.9 2.4 12.1 3.6 18.3 3.6 8.2 0 16.3-2.1 23.6-6.2 12.8-7.2 21.6-20 23.9-34.5l59.4-387.2c6.1-40.1-36.9-68.8-71.5-48.9zM192 464v-64.6l36.6 15.1L192 464zm212.6-28.7l-153.8-63.5L391 169.5c10.7-15.5-9.5-33.5-23.7-21.2L155.8 332.6 48 288 464 48l-59.4 387.3z"
+        ></path>
+      </svg>
+    </button>
   </div>
 </template>
 
@@ -61,19 +76,18 @@ const dummyUsers = [
 ]
 import SideBar from './../components/SideBar'
 import OnlineUser from './../components/OnlineUser'
-// import PublicRoom from './../components/PublicRoom'
 
 export default {
   name: 'PublicMessage',
   components: {
     SideBar,
     OnlineUser,
-    // PublicRoom,
   },
   data() {
     return {
       onlineCount: -1,
       users: [],
+      message: '',
     }
   },
   created() {
@@ -96,61 +110,85 @@ export default {
 <style scoped>
 .wrapper {
   display: grid;
-  grid-template-columns: 378px 378px 1fr;
-  grid-template-rows: 100vh;
+  height: 100vh;
+  grid-template-columns: 378px 378px 1fr 50px;
+  grid-template-rows: 55px 1fr 80px;
 }
 
 .side-bar {
   position: fixed;
   z-index: 2;
   grid-column: 1 / 2;
-  grid-row: 1 / 2;
+  grid-row: 1 / 4;
   padding-left: 103px;
   width: 378px;
   height: 100vh;
   border-right: 1px solid #e6ecf0;
 }
 
-.center-column {
-  grid-column: 2 / 3;
-  grid-row: 1 / 2;
-  border-right: 1px solid #e6ecf0;
-}
-
-.header {
-  position: fixed;
-  z-index: 1;
-  height: 55px;
-  background: #fff;
-}
-
-.header-title {
-  margin-left: 15px;
-  line-height: 55px;
-
+.header-users,
+.header-room {
+  padding: 14px 0 0 15px;
   font-family: 'Noto Sans TC', sans-serif;
   font-weight: 700;
   font-size: 18px;
   color: #1c1c1c;
-}
-
-.onlineUser-wrapper {
-  margin-top: 55px;
-  border-top: 1px solid #e6ecf0;
-}
-
-.onlineUser {
-  width: 378px;
+  background: #fff;
+  border-right: 1px solid #e6ecf0;
   border-bottom: 1px solid #e6ecf0;
 }
 
-.right-column {
-  grid-column: 3 / 4;
+.header-users {
+  grid-column: 2 / 3;
   grid-row: 1 / 2;
 }
 
-.publicRoom {
-  margin-top: 55px;
-  border-top: 1px solid #e6ecf0;
+.onlineUser-wrapper {
+  grid-column: 2 / 3;
+  grid-row: 2 / 4;
+  width: 378px;
+  border-right: 1px solid #e6ecf0;
+}
+
+.onlineUser {
+  border-bottom: 1px solid #e6ecf0;
+}
+
+.header-room {
+  grid-column: 3 / 5;
+  grid-row: 1 / 2;
+}
+
+.messages-wrapper {
+  grid-column: 3 / 5;
+  grid-row: 2 / 3;
+  border-bottom: 1px solid #e6ecf0;
+}
+
+/* .input-wrapper {
+  display: flex;
+  align-items: center;
+} */
+
+.input-text {
+  grid-column: 3 / 4;
+  grid-row: 3 / 4;
+  margin: 15px 0 0 15px;
+  padding-left: 15px;
+  height: 50px;
+  border-radius: 50px;
+  background: #e6ecf0;
+}
+
+.input-icon {
+  grid-column: 4 / 5;
+  grid-row: 3 / 4;
+  margin: 25px 0 0 10px;
+  width: 24px;
+  height: 24px;
+}
+
+.input-icon path {
+  fill: #ff6600;
 }
 </style>
